016dd64c32888030cffe30527181fc476fc9c7b6
Shanly Rajan <shanly.rajan@ska.ac.za>
2012-10-18 10:30:50 +0200

    power down logic merged with Marc

--- arch/powerpc/platforms/44x/roach2.c
+++ arch/powerpc/platforms/44x/roach2.c
@@ -23,72 +23,80 @@
 #include <linux/of_gpio.h>
 
 static __initdata struct of_device_id roach2_of_bus[] = {
-	{ .compatible = "ibm,plb4", },
-	{ .compatible = "ibm,opb", },
-	{ .compatible = "ibm,ebc", },
-	{ .compatible = "simple-bus", },
-	{},
+        { .compatible = "ibm,plb4", },
+        { .compatible = "ibm,opb", },
+        { .compatible = "ibm,ebc", },
+        { .compatible = "simple-bus", },
+        {},
 };
 
 static int __init roach2_device_probe(void)
 {
-	of_platform_bus_probe(NULL, roach2_of_bus, NULL);
+        of_platform_bus_probe(NULL, roach2_of_bus, NULL);
 
-	return 0;
+        return 0;
 }
 machine_device_initcall(roach2, roach2_device_probe);
 
 static int __init roach2_probe(void)
 {
-	unsigned long root = of_get_flat_dt_root();
+        unsigned long root = of_get_flat_dt_root();
 
-	if (of_flat_dt_is_compatible(root, "kat,roach2")) {
-		pci_set_flags(PCI_REASSIGN_ALL_RSRC);
-		return 1;
-	}
+        if (of_flat_dt_is_compatible(root, "kat,roach2")) {
 
-	return 0;
+                printk(KERN_INFO "roach2: claiming matching platform");
+
+                pci_set_flags(PCI_REASSIGN_ALL_RSRC);
+
+                return 1;
+        }
+
+        return 0;
 }
 
 static void roach2_power_off(void)
 {
         struct device_node *np, *child;
-	int pin;
+        int pin;
 
         pin = (-1);
 
-        printk(KERN_INFO "attempting hardware power down");
+        printk(KERN_INFO "roach2: performing hardware power down");
 
         np = of_find_compatible_node(NULL, NULL, "gpio-pins");
         if (!np) {
-                printk(KERN_ERR "unable to locate gpio interface");
-		return;
+                printk(KERN_ERR "roach2: unable to locate gpio interface");
+                return;
         }
 
         for_each_child_of_node(np, child)
                 if (strcmp(child->name, "powerdown") == 0) {
                         pin = of_get_gpio(child, 0);
-		}
+                }
 
-	of_node_put(np);
+        of_node_put(np);
 
         if(pin < 0){
-		printk(KERN_WARNING "no gpio for power circuit found");
-		return;
-	}
+                printk(KERN_WARNING "roach2: no gpio for power circuit found");
+                return;
+        }
 
-        printk(KERN_INFO "about to drive pin low");
 
-	gpio_set_value(pin, 0);
+        printk(KERN_DEBUG "roach2: about to drive power pin low");
+        gpio_set_value(pin, 0);
+
+        printk(KERN_DEBUG "roach2: about to drive power pin high");
+        gpio_set_value(pin, 1);
+
 }
 
 define_machine(roach2) {
-	.name = "PowerPC 44x Platform",
-	.probe = roach2_probe,
-	.progress = udbg_progress,
-	.init_IRQ = uic_init_tree,
-	.get_irq = uic_get_irq,
-	.restart = ppc4xx_reset_system,
-	.power_off = roach2_power_off,
-	.calibrate_decr = generic_calibrate_decr,
+        .name = "PowerPC 44x Platform",
+                .probe = roach2_probe,
+                .progress = udbg_progress,
+                .init_IRQ = uic_init_tree,
+                .get_irq = uic_get_irq,
+                .restart = ppc4xx_reset_system,
+                .power_off = roach2_power_off,
+                .calibrate_decr = generic_calibrate_decr,
 };
